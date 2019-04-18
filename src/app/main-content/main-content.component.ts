3b44b4b14d900d35d8bee36ea600cd9571108101
DoctorWoozy <connormillersmail@gmail.com>
2019-04-18 13:24:08 -0700

    finish lab 5

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,13 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 
-interface IStudent {
-  id: number;
-  firstName: string;
-  lastName: string;
-  course: string;
-  editMode: boolean;
-}
 
 @Component({
   selector: 'app-main-content',
@@ -16,62 +9,8 @@ interface IStudent {
 })
 export class MainContentComponent implements OnInit {
 
-  private studentArray: Array<IStudent> = [];
-  disableAddButton = false;
   constructor() { }
 
   ngOnInit() {
-    this.studentArray = [
-      {
-        id: 1,
-        firstName: 'Connor',
-        lastName: 'Miller',
-        course: 'Programming',
-        editMode: false
-      },
-      {
-        id: 2,
-        firstName: 'Mike',
-        lastName: 'Tyson',
-        course: 'Boxing',
-        editMode: false
-      },
-      {
-        id: 3,
-        firstName: 'Mohammed',
-        lastName: 'Ali',
-        course: 'Super Boxing',
-        editMode: false
-      }
-    ];
   }
-
-  addStudent() {
-    this.studentArray.unshift({
-      id: null,
-      firstName: null,
-      lastName: null,
-      course: null,
-      editMode: true
-    });
-    this.disableAddButton = true;
-  }
-
-  removeStudent(index: number) {
-    this.studentArray.splice(index, 1);
-  }
-
-  saveStudent() {
-    this.studentArray[0].editMode = false;
-    this.disableAddButton = false;
-
-    this.sort('asc');
-  }
-
-  sort(direction: string) {
-    this.studentArray.sort((a: IStudent, b: IStudent, ) => {
-      return a.id < b.id ? -1 : 1;
-    });
-  }
-
 }
